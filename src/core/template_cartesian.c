840bb9d1596b7fd4989dbf060456a66b6d358d3a
Romulo Rodrigues <romulo.rodrigues@kuleuven.be>
2023-02-03 18:16:22 +0100

    Checking if size to allocate is greater than zero

--- src/core/template_cartesian.c
+++ src/core/template_cartesian.c
@@ -15,10 +15,18 @@ void template_cartesian_allocate_memory(template_cartesian_t *template,
     point2d_array_t *side[3] = {&template->left, &template->front, &template->right};
 
     if (ALLOCATION_MODE == 1){
-        for(int i=0; i<3; i++){
-            side[i]->number_of_points = 0;
-            side[i]->max_number_of_points = max_number_of_samples[0];    
-            side[i]->points = (point2d_t *) malloc(max_number_of_samples[0] * sizeof(point2d_t));        
+        if (max_number_of_samples[0] > 0){
+            for(int i=0; i<3; i++){
+                side[i]->number_of_points = 0;
+                side[i]->max_number_of_points = max_number_of_samples[0];    
+                side[i]->points = (point2d_t *) malloc(max_number_of_samples[0] * sizeof(point2d_t));  
+                if (side[i]->points != NULL){
+                    side[i]->max_number_of_points = max_number_of_samples[0];
+                }else{
+                    // Failed to allocate memory
+                    side[i]->max_number_of_points = 0;
+                }      
+            } // for
         }
     }
 }
